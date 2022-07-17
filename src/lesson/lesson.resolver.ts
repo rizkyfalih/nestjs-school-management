97c5304c6bc5e34009a7d7dc7970b5c6ba9f7ba7
rizky pridyandhika <rizkypridyandhika@rizkys-MacBook-Pro.local>
2022-07-17 09:51:25 +0700

    add lesson module

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,21 @@
+import { Mutation, Query, Resolver } from "@nestjs/graphql";
+import { LessonType } from "./lesson.type";
+
+@Resolver(of => LessonType)
+export class LessonResolver {
+    @Query(returns => LessonType)
+    lesson(){
+        return {
+            id: 'abscdsfe',
+            name: 'Physics Class',
+            startDate: (new Date()).toISOString(),
+            endDate: (new Date()).toISOString(),
+        };
+    }
+
+    @Mutation(returns => LessonType)
+    createLesson() {
+        
+    }
+
+}
